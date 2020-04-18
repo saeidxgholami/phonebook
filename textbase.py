666b62be10e89a1906f2c58c25a9705fc271391c
Saeid Gholami <saeidxgholami@gmail.com>
2020-04-18 21:15:33 +0430

    create function for each action in textbase version

--- textbase.py
+++ textbase.py
@@ -1,63 +1,91 @@
+# Text(Termina, console) base version of PhoneBook application
+# Features: Create, Find, Update, Delete contact and Display contacts
+# author: @saeidxgholami
+
 import contact
 
 
 
 def menu():
 	menu_items = [
-		"1. Create a contact",
-		"2. Display Contacts",
-		"3. Find a Contact",
-		"4. Update a  Contact",
-		"5. Delete a  Contact",
-		"6. Quit",
+		"Create a contact",
+		"Display Contacts",
+		"Find a Contact",
+		"Update a  Contact",
+		"Delete a  Contact",
+		"Quit",
 	]
-	for item in menu_items:
-		print(item)
+	for index, item in enumerate(menu_items, 1):
+		print(f"[{index}] {item}")
 
 	response = input("Please choose from menu:\n>>  ")
 	return response
-# Text(Termina, console) base version of PhoneBook application
-# Features: Create, Find, Update, Delete contact and Display contacts
-# author: @saeidxgholami
+
+
+def create_contact():
+	name = input("Name: ")
+	phone = input("Phone: ")
+	if contact.create(name, phone):
+		print('Contact created successfuly.')
+	else:
+		print('Error')
+
+
+def display_contacts():
+	for name, phone in contact.read():
+		print(name, phone) 
+
+
+def find_contact():
+	name = input("Enter a name to find: ")
+	found = contact.find(name)
+	if found:
+		print(contact.contacts[name])
+	else:
+		print('not found')
+
+def update_contact():
+	name = input("Enter a name to update: ")
+	found = contact.find(name)
+	if found:
+		new_name = input("New Name: ")
+		new_phone = input("New Phone: ")
+		if contact.update(name, new_name, new_phone):
+			print('Updaetd!')
+		else:
+			print('Error in update!')
+	else:
+		print('Contact not found.')
+
+
+def delete_contact():
+	name = input("Enter a name to update: ")
+	found = contact.find(name)
+	if found:
+		contact.delete(name)
+	else:
+		print('not found')
 
 
 def main():
 	while True:
 		response = menu()
+		# Create contact
 		if response == '1':
-			name = input("Name: ")
-			phone = input("Phone: ")
-			contact.create(name, phone)
-			print('Contact created successfuly.')
+			create_contact()
+		# display contacts
 		elif response == '2':
-			for name, phone in contact.read():
-				print(name, phone) 
+			display_contacts()
+		# find contacts
 		elif response == '3':
-			name = input("Enter a name to find: ")
-			found = contact.find(name)
-			if found:
-				print(contact.contacts[name])
-			else:
-				print('not found')
+			find_contact()
+		# update a contact
 		elif response == '4':
-			name = input("Enter a name to update: ")
-			found = contact.find(name)
-			if found:
-				new_name = input("New Name: ")
-				new_phone = input("New Phone: ")
-				if contact.update(name, new_name, new_phone):
-					print('Updaetd!')
-				else:
-					print('Error in update!')
-			else:
-				print('Contact not found.')
+			update_contact()
+		# delete a contact
 		elif response == '5':
-			name = input("Enter a name to update: ")
-			found = contact.find(name)
-			if found:
-				contact.delete(name)
-			else:
-				print('not found')
+			delete_contact()
+		# quit
 		elif response == '6':
 			break
 
